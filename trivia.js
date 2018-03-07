ff0af6da7a885905664084c0cac24fe9ca7e213c
okputadora <okputadora@gmail.com>
2018-03-07 17:25:04 -0500

    basic gameflow and a timer

--- trivia.js
+++ trivia.js
@@ -2,11 +2,12 @@
     {
       question: 'life, head, heart, and destiny are the four main topics what practice?',
        possibleAnswers: ['astrology', 'eschatology', 'palm-reading', 'numerology'],
-       answer: 'palm-reading'}
+       answer: 'palm-reading'
+     },
     {
       question: 'If you had one bill (i.e. paper money not coins) with each President on Mt. Rushmore, how much money would you have?',
       possibleAnswers: ['$1', '$5','$6', '$7'],
-      answer: ['$7']
+      answer: '$7'
     },
     {
       question: 'What is secreted by lachrymose glands?',
@@ -16,33 +17,63 @@
     {
       question: 'What would you find in a ossuary?',
       possibleAnswers: ['bones', 'bears', 'bells', 'birds'],
-      answer: ['bones']
+      answer: 'bones'
     },
     {
       question: 'In what country was basketball invented?',
       possibleAnswers: ['Australia', 'USA', 'Canada', 'Sweden'],
-      answer:
+      answer: 'Canada'
     }
   ]
 
-
+  var button = $("<button>")
+  button.addClass("btn btn-default")
+  var instruction = $("<h1>")
+  var clock = $("<div>").addClass("clock")
 
   $(document).ready(function(){
-    $(".ready").on("click", function(){
-
-    })
+    $("#ready").on("click", startGame)
   })
 
-  function questionAndAnswers() {
-           for (var i=0; i<questionData.length; i++) {
-             var questionElements = $("<div>");
-             questionElements.addClass("question-box")
-             questionElements.html("<p>" + questionData[i].question + "</p>");
-             $("#question-box").append(questionElements);
-             var questionPossibleAnswers = $('<input type="radio" name="answer-choice" value=' + [i] + '/>');
-             questionPossibleAnswers.addClass("question-box");
-             questionPossibleAnswers.attr("value");
-             questionPossibleAnswers.html(questionData.possibleAnswers[i]);
-             $("#question-box").append(questionPossibleAnswers);
-           }
-       }
+  function startGame(){
+    // initialize the variables we'll need to track from round to round
+    var round = 0
+    var correctCount = 0
+    //move the scoreboard to the top
+    $(".scoreboard").css("margin-top", "0px")
+    $(".game").css("opacity", "1");
+    // start the round
+    startRound(round, correctCount)
+  }
+
+  function startRound(roundNum, correctNum){
+    // if we have questions left
+    if (roundNum <= 4){
+      loadScoreBoard(roundNum, 10)
+      loadGame(roundNum)
+      return
+    }
+    // display results
+  }
+
+  function loadScoreBoard(number, time){
+    instruction.html("Question " + (number + 1))
+    $(".scoreboard").html(instruction)
+    $(".scoreboard").append(clock)
+    // contdown
+    var timer = setInterval(function(){
+      // if countdown done
+      if (time == 0){
+        clearInterval(timer)
+        // go to the next question
+      }
+      $(".clock").html(time)
+      time--
+    }, 1000)
+
+  }
+
+  function loadGame(index){
+    question = trivia[index].question
+    console.log(question)
+  }
